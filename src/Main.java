0d5124be77336f61f398e23a7dc1c7d3afa6cd38
luctia <timmerman.luc1999@gmail.com>
2020-12-01 14:50:40 +0100

    Day 1 part 2 implementation

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Day1 day1 = new Day1();
         System.out.println(day1.part1());
+        System.out.println(day1.part2());
     }
 }
--- src/solutions/Day1.java
+++ src/solutions/Day1.java
@@ -26,9 +26,22 @@ public class Day1 {
 
     public int part1() {
         int sum = 0;
+        HashSet<Integer> modules = getData();
+        for (Integer module : modules) {
+            sum += Math.floorDiv(module, 3) - 2;
+        }
+        return sum;
+    }
+
+    public int part2() {
+        int sum = 0;
         HashSet<Integer> data = getData();
-        for (Integer entry : data) {
-            sum += Math.floorDiv(entry ,3) - 2;
+        for (Integer module : data) {
+            int newfuel = Math.floorDiv(module, 3) - 2;
+            while (newfuel > 0) {
+                sum += newfuel;
+                newfuel = Math.floorDiv(newfuel, 3) - 2;
+            }
         }
         return sum;
     }
