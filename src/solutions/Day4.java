2824a15b6680a444f1e44cd6437a6c9b504b054b
luctia <timmerman.luc1999@gmail.com>
2020-12-03 20:26:52 +0100

    Day 4 part 2

--- src/solutions/Day4.java
+++ src/solutions/Day4.java
@@ -26,20 +26,28 @@ public class Day4 {
             return res;
         }
 
-        private boolean isValidPassword(int password) {
+        private static boolean isValidPassword(int password) {
             String pw = Integer.toString(password);
             int prev = 0;
             int cons = 0;
             for (char c : pw.toCharArray()) {
                 int parsed = Integer.parseInt(""+c);
-                if (parsed == prev) {
-                    cons++;
-                } else if (parsed < prev) {
+                if (parsed < prev) {
                     return false;
+                } else if (parsed == prev && countInString(pw, c) == 2) {
+                    cons++;
                 }
                 prev = parsed;
             }
             return cons > 0;
+        }
+
+        private static int countInString(String input, char needle) {
+            int res = 0;
+            for (char c : input.toCharArray()) {
+                if (c == needle) res++;
+            }
+            return res;
         }
     }
 }
