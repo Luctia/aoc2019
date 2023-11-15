ca0a3b558402af786b6a9380eb4cc498285e178e
luctia <timmerman.luc1999@gmail.com>
2023-11-15 18:26:55 +0100

    Day 14 part 1

--- src/Main.java
+++ src/Main.java
@@ -6,58 +6,62 @@ import solutions.*;
  */
 public class Main {
     public static void main(String[] args) {
-        Day1 day1 = new Day1();
-        System.out.println(day1.part1());
-        System.out.println(day1.part2());
+//        Day1 day1 = new Day1();
+//        System.out.println(day1.part1());
+//        System.out.println(day1.part2());
+//
+//        Day2 day2 = new Day2();
+//        System.out.println(day2.part1());
+//        System.out.println(day2.part2());
+//
+//        Day3 day3 = new Day3();
+//        try {
+//            day3.part1();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        Day4 day4 = new Day4();
+//        day4.part1();
+//
+//        Day5 day5 = new Day5();
+//        System.out.println(day5.part1());
+//        System.out.println(day5.part2());
+//
+//        Day6 day6 = new Day6();
+//        System.out.println(day6.part1());
+//        System.out.println(day6.part2());
+//
+//        Day7 day7 = new Day7();
+//        System.out.println(day7.part1());
+//        System.out.println(day7.part2());
+//
+//        Day8 day8 = new Day8();
+//        System.out.println(day8.part1());
+//        day8.part2();
+//
+//        Day9 day9 = new Day9();
+//        System.out.println(day9.part1());
+//        System.out.println(day9.part2());
+//
+//        Day10 day10 = new Day10();
+//        System.out.println(day10.part1());
+//        System.out.println(day10.part2());
+//
+//        Day11 day11 = new Day11();
+//        System.out.println(day11.part1());
+//        System.out.println(day11.part2());
+//
+//        Day12 day12 = new Day12();
+//        System.out.println(day12.part1());
+//        System.out.println(day12.part2());
+//
+//        Day13 day13 = new Day13();
+//        System.out.println(day13.part1());
+//        System.out.println(day13.part2());
 
-        Day2 day2 = new Day2();
-        System.out.println(day2.part1());
-        System.out.println(day2.part2());
-
-        Day3 day3 = new Day3();
-        try {
-            day3.part1();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Day4 day4 = new Day4();
-        day4.part1();
-
-        Day5 day5 = new Day5();
-        System.out.println(day5.part1());
-        System.out.println(day5.part2());
-
-        Day6 day6 = new Day6();
-        System.out.println(day6.part1());
-        System.out.println(day6.part2());
-
-        Day7 day7 = new Day7();
-        System.out.println(day7.part1());
-        System.out.println(day7.part2());
-
-        Day8 day8 = new Day8();
-        System.out.println(day8.part1());
-        day8.part2();
-
-        Day9 day9 = new Day9();
-        System.out.println(day9.part1());
-        System.out.println(day9.part2());
-
-        Day10 day10 = new Day10();
-        System.out.println(day10.part1());
-        System.out.println(day10.part2());
-
-        Day11 day11 = new Day11();
-        System.out.println(day11.part1());
-        System.out.println(day11.part2());
-
-        Day12 day12 = new Day12();
-        System.out.println(day12.part1());
-        System.out.println(day12.part2());
-
-        Day13 day13 = new Day13();
-        System.out.println(day13.part1());
-        System.out.println(day13.part2());
+        Day14 day14 = new Day14();
+        System.out.println(day14.part1());
+//        System.out.println(day14.part2());
     }
 }
---
+++ src/solutions/Day14.java
@@ -0,0 +1,109 @@
+package solutions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Day14 {
+    public long part1() {
+        List<Reaction> reactions = new ArrayList<>();
+        try {
+            File data = new File("src/data/day14.txt");
+            Scanner reader = new Scanner(data);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                reactions.add(new Reaction(line));
+            }
+            return calculateMinimalOre(reactions, new HashMap<>(), "FUEL", 1).amount;
+        } catch (FileNotFoundException e) {
+            System.out.println("Day 14 data file not found.");
+        }
+        return -1;
+    }
+
+    /**
+     * Calculates the minimal amount of ore required to create a certain material, given some recipes.
+     * @param reactions the possible reactions
+     * @param leftovers what we have left
+     * @param material the material to create
+     * @param amountNeeded how much of the material we need
+     * @return the minimal amount of ore required to create the material
+     */
+    private ReactionResult calculateMinimalOre(List<Reaction> reactions, Map<String, Long> leftovers, String material, long amountNeeded) {
+        List<Long> amounts = new ArrayList<>();
+        List<Map<String, Long>> leftOverList = new ArrayList<>();
+        List<Reaction> reactionsResultingInMaterial = reactions.stream().filter(r -> r.result.equals(material)).toList();
+        // We need *material*, so we look through all reactions that have that as a result.
+        for (Reaction reaction : reactionsResultingInMaterial) {
+            // We reset the leftovers for every different recipe.
+            long oreRequired = 0;
+            long repeatReactionTimes = Math.ceilDiv(amountNeeded, reaction.resultAmount);
+            long resultingAmount = repeatReactionTimes * reaction.resultAmount;
+            for (int i = 0; i < reaction.requiredAmounts.size(); i++) {
+                // For every ingredient...
+                String ingredient = reaction.requiredComponents.get(i);
+                long ingredientAmount = reaction.requiredAmounts.get(i);
+                if (ingredient.equals("ORE")) {
+                    oreRequired += ingredientAmount * repeatReactionTimes;
+                } else {
+                    // If it's not ore, we need another reaction to get this ingredient
+                    // We need *ingredientAmount* of this ingredient for this recipe
+                    // However, we should check whether we already have some of this ingredient left over.
+                    if (leftovers.containsKey(ingredient) && leftovers.get(ingredient) >= (ingredientAmount * repeatReactionTimes)) {
+                        // We already have enough of this ingredient. Thus, take it from the leftovers and don't add any
+                        //  extra required ore.
+                        leftovers.put(ingredient, leftovers.get(ingredient) - (ingredientAmount * repeatReactionTimes));
+                    } else {
+                        // While we might still have some of this ingredient left over, it's not enough, so we must make
+                        //  more. To be precise, we must make *amountNeeded* - what we have left over.
+                        long amountLeftOver = leftovers.getOrDefault(ingredient, 0L);
+                        // If we have some material left over, we should take all that. This means we need to remove it from our leftovers.
+                        leftovers.put(ingredient, 0L);
+                        ReactionResult result = calculateMinimalOre(reactions, leftovers, ingredient, (ingredientAmount * repeatReactionTimes) - amountLeftOver);
+                        oreRequired += result.amount;
+                        leftovers = result.leftovers;
+                    }
+                }
+            }
+            leftovers.put(material, leftovers.getOrDefault(material, 0L) + resultingAmount - amountNeeded);
+            amounts.add(oreRequired);
+            leftOverList.add(leftovers);
+        }
+        long minimum = amounts.get(0);
+        Map<String, Long> correspondingLeftovers = leftOverList.get(0);
+        for (int i = 1; i < amounts.size(); i++) {
+            if (amounts.get(i) < minimum) {
+                minimum = amounts.get(i);
+                correspondingLeftovers = leftOverList.get(0);
+            }
+        }
+        return new ReactionResult(minimum, correspondingLeftovers);
+    }
+
+    private record ReactionResult(long amount, Map<String, Long> leftovers) {
+        public ReactionResult(long amount, Map<String, Long> leftovers) {
+            this.amount = amount;
+            this.leftovers = leftovers;
+        }
+    }
+
+    private static class Reaction {
+        private final String result;
+        private final int resultAmount;
+        private final List<String> requiredComponents;
+        private final List<Integer> requiredAmounts;
+
+        public Reaction(String reaction) {
+            String result = reaction.split(" => ")[1];
+            this.resultAmount = Integer.parseInt(result.split(" ")[0]);
+            this.result = result.split(" ")[1];
+            this.requiredAmounts = new ArrayList<>();
+            this.requiredComponents = new ArrayList<>();
+            String[] requirements = reaction.split("=>")[0].split(", ");
+            for (String requirement : requirements) {
+                this.requiredComponents.add(requirement.split(" ")[1]);
+                this.requiredAmounts.add(Integer.parseInt(requirement.split(" ")[0]));
+            }
+        }
+    }
+}
